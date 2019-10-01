43fcfc499197e5e707344f5b88246bb3da2cf048
Derick Mwenda <52851761+d-mwenda@users.noreply.github.com>
2019-10-01 16:33:14 +0300

    version 0.02 - redirect after login changed to asset list, redirect after logout changed to login, root url redirected to asset list, currently irrelevant menu options (search, issue, store, withdraw) removed by commenting them out, footer fixed, top navigation removed from login page

--- atbs/index/views.py
+++ atbs/index/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from django.shortcuts import redirect
+from django.urls import reverse_lazy
 
 
 class Dashboard(TemplateView):
     template_name = 'index/dashboard.html'
+
+    def get(self, request, *args, **kwargs):
+        response = redirect(reverse_lazy('login'))
+        return response
 
 
 class About(TemplateView):
