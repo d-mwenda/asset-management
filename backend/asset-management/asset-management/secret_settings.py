e569c19b097772ed394bde14bc224246b7a9af11
Derick Mwenda <52851761+d-mwenda@users.noreply.github.com>
2020-06-18 16:11:26 +0300

    Ionic Support
    Ionic support added and ionic with angular app initialized.

---
+++ backend/asset-management/asset-management/secret_settings.py
@@ -0,0 +1,10 @@
+__author__ = 'Derick Mwenda'
+
+db_params = {
+    'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    'NAME': 'atbs',
+    'USER': 'atbs_dba',
+    'PASSWORD': 'kitumbu@ ku1ng1@ Mchanga',
+    'HOST': 'localhost',
+    'PORT': '5432',
+}
--- frontend/asset-management/src/app/app-routing.module.ts
+++ frontend/asset-management/src/app/app-routing.module.ts
@@ -1,11 +1,22 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'folder/Inbox',
+    pathMatch: 'full'
+  },
+  {
+    path: 'folder/:id',
+    loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
